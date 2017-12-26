69f30c915ce763537c2fc815483c9102e5f28d25
Harshvardhan Takawale <harshvardhantakawale@gmail.com>
2017-12-26 09:57:38 +0530

    Landing page

--- app/src/main/java/com/dota/arena18/activities/LandingActivity.java
+++ app/src/main/java/com/dota/arena18/activities/LandingActivity.java
@@ -1,11 +1,18 @@
 package com.dota.arena18.activities;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.DecelerateInterpolator;
 
 import com.dota.arena18.R;
+import com.gjiazhe.panoramaimageview.GyroscopeObserver;
+import com.gjiazhe.panoramaimageview.PanoramaImageView;
 
 public class LandingActivity extends AppCompatActivity {
 
@@ -24,11 +31,38 @@ public class LandingActivity extends AppCompatActivity {
     // TODO: All items should be in a Recycler, instead of as individual buttons.
     // TODO: Copy over current OnClick functions to Recycler when implemented
 
+    private GyroscopeObserver gyroscopeObserver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+
+
+        gyroscopeObserver = new GyroscopeObserver();
+
+        gyroscopeObserver.setMaxRotateRadian(Math.PI/2);
+
+        PanoramaImageView panoramaImageView = (PanoramaImageView) findViewById(R.id.panorama_image_view);
+        panoramaImageView.setGyroscopeObserver(gyroscopeObserver);
+
+
+
+
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        gyroscopeObserver.unregister();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        gyroscopeObserver.register(this);
+    }
+
 
     public void handleClicks(View v){
         switch (v.getId()){
