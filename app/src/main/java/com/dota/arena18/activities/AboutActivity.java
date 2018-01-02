83f59689ea4d75899a2a68f3a5a9e6d046f86457
TheGamer007 <shriakhilc@gmail.com>
2018-01-02 19:21:48 +0530

    Add location perms before MapsActivity
    The location permission isn't needed to display the map, and we are currently not displaying user's location. If we do not add that feature, then this perm check and the location permission can be removed.

--- app/src/main/java/com/dota/arena18/activities/AboutActivity.java
+++ app/src/main/java/com/dota/arena18/activities/AboutActivity.java
@@ -1,11 +1,18 @@
 package com.dota.arena18.activities;
 
+import android.Manifest;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 import com.dota.arena18.R;
+
+import static android.support.v4.content.PermissionChecker.PERMISSION_DENIED;
+import static android.support.v4.content.PermissionChecker.PERMISSION_GRANTED;
 
 public class AboutActivity extends AppCompatActivity {
     /**
@@ -14,6 +21,8 @@ public class AboutActivity extends AppCompatActivity {
      *
      */
 
+    private static final int REQUEST_LOCATION = 101;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +30,12 @@ public class AboutActivity extends AppCompatActivity {
     }
 
     public void openMap(View v){
-        startActivity(new Intent(this, MapsActivity.class));
+        int permCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permCheck == PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+        } else {
+            startActivity(new Intent(this, MapsActivity.class));
+        }
     }
 
     public void openAbout(View v){
@@ -34,5 +48,20 @@ public class AboutActivity extends AppCompatActivity {
         Intent i = new Intent(this, TextDisplayActivity.class);
         i.putExtra("text", "dir");
         startActivity(i);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case REQUEST_LOCATION:
+                if (grantResults.length > 0 && grantResults[0] == PERMISSION_GRANTED) {
+                    startActivity(new Intent(this, MapsActivity.class));
+                } else {
+                    // Permissions not granted; display reasoning, or deactivate feature
+                }
+                break;
+            default: super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
     }
 }
