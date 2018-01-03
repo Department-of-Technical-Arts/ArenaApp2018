8442c0d3c286a66cd4893d3b813072b3ffe5d313
TheGamer007 <shriakhilc@gmail.com>
2018-01-03 16:10:36 +0530

    Change TableView to SortableTableView
    * Columns of the medals tally table can now be sorted in asc/desc order
    * Individual column comparators implemented for this purpose

--- app/src/main/java/com/dota/arena18/activities/MedalsTallyActivity.java
+++ app/src/main/java/com/dota/arena18/activities/MedalsTallyActivity.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
-import de.codecrafters.tableview.TableView;
+import de.codecrafters.tableview.SortableTableView;
 import de.codecrafters.tableview.model.TableColumnWeightModel;
 import de.codecrafters.tableview.toolkit.SimpleTableDataAdapter;
 import de.codecrafters.tableview.toolkit.SimpleTableHeaderAdapter;
@@ -41,7 +41,7 @@ public class MedalsTallyActivity extends AppCompatActivity {
 
     private final String[] TABLE_HEADERS = {HEADER_RANK, HEADER_NAME, HEADER_GOLD, HEADER_SILVER, HEADER_BRONZE, HEADER_OTHERS};
 
-    private TableView<String[]> tableView;
+    private SortableTableView<String[]> sortableTableView;
     private ArrayList<CollegeDetails> tableData;
 
     @Override
@@ -49,7 +49,7 @@ public class MedalsTallyActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medals_tally);
 
-        tableView = findViewById(R.id.table_tally);
+        sortableTableView = findViewById(R.id.table_tally);
 
         TableColumnWeightModel columnModel = new TableColumnWeightModel(6);
         columnModel.setColumnWeight(COLUMN_RANK, 1);
@@ -58,7 +58,79 @@ public class MedalsTallyActivity extends AppCompatActivity {
         columnModel.setColumnWeight(COLUMN_SILVER, 1);
         columnModel.setColumnWeight(COLUMN_BRONZE, 1);
         columnModel.setColumnWeight(COLUMN_OTHERS, 1);
-        tableView.setColumnModel(columnModel);
+        sortableTableView.setColumnModel(columnModel);
+
+        // Add comparators to define sorting
+        sortableTableView.setColumnComparator(COLUMN_RANK, new Comparator<String[]>() {
+            @Override
+            public int compare(String[] strings1, String[] strings2) {
+                int rank1 = Integer.parseInt(strings1[COLUMN_RANK]);
+                int rank2 = Integer.parseInt(strings2[COLUMN_RANK]);
+
+                // -1 implies order rank1 before rank2
+                // 1 implies rank2 before rank1
+                // 0 implies equal
+
+                if (rank1 < rank2) return -1;
+                else if (rank1 > rank2) return 1;
+                else return 0;
+            }
+        });
+
+        sortableTableView.setColumnComparator(COLUMN_NAME, new Comparator<String[]>() {
+            @Override
+            public int compare(String[] strings1, String[] strings2) {
+                return strings1[COLUMN_NAME].compareToIgnoreCase(strings2[COLUMN_NAME]);
+            }
+        });
+
+        sortableTableView.setColumnComparator(COLUMN_GOLD, new Comparator<String[]>() {
+            @Override
+            public int compare(String[] strings1, String[] strings2) {
+                int count1 = Integer.parseInt(strings1[COLUMN_GOLD]);
+                int count2 = Integer.parseInt(strings2[COLUMN_GOLD]);
+
+                if (count1 < count2) return -1;
+                else if (count1 > count2) return 1;
+                else return 0;
+            }
+        });
+
+        sortableTableView.setColumnComparator(COLUMN_SILVER, new Comparator<String[]>() {
+            @Override
+            public int compare(String[] strings1, String[] strings2) {
+                int count1 = Integer.parseInt(strings1[COLUMN_SILVER]);
+                int count2 = Integer.parseInt(strings2[COLUMN_SILVER]);
+
+                if (count1 < count2) return -1;
+                else if (count1 > count2) return 1;
+                else return 0;
+            }
+        });
+
+        sortableTableView.setColumnComparator(COLUMN_BRONZE, new Comparator<String[]>() {
+            @Override
+            public int compare(String[] strings1, String[] strings2) {
+                int count1 = Integer.parseInt(strings1[COLUMN_BRONZE]);
+                int count2 = Integer.parseInt(strings2[COLUMN_BRONZE]);
+
+                if (count1 < count2) return -1;
+                else if (count1 > count2) return 1;
+                else return 0;
+            }
+        });
+
+        sortableTableView.setColumnComparator(COLUMN_OTHERS, new Comparator<String[]>() {
+            @Override
+            public int compare(String[] strings1, String[] strings2) {
+                int count1 = Integer.parseInt(strings1[COLUMN_OTHERS]);
+                int count2 = Integer.parseInt(strings2[COLUMN_OTHERS]);
+
+                if (count1 < count2) return -1;
+                else if (count1 > count2) return 1;
+                else return 0;
+            }
+        });
 
         testResponse();
     }
@@ -83,9 +155,9 @@ public class MedalsTallyActivity extends AppCompatActivity {
     void loadData() {
 //        String[][] DATA_TO_SHOW = {{"1", "BITS Hyderabad", "2", "2", "1", "1"}, {"2", "Harvard University", "2", "0", "1", "0"}};
 
-//        tableView.setHeaderAdapter(new MedalsTableHeaderAdapter(this));
-        tableView.setHeaderAdapter(new SimpleTableHeaderAdapter(this, TABLE_HEADERS));
-        tableView.setDataAdapter(new SimpleTableDataAdapter(this, getList(tableData)));
+//        sortableTableView.setHeaderAdapter(new MedalsTableHeaderAdapter(this));
+        sortableTableView.setHeaderAdapter(new SimpleTableHeaderAdapter(this, TABLE_HEADERS));
+        sortableTableView.setDataAdapter(new SimpleTableDataAdapter(this, getList(tableData)));
     }
 
     ArrayList<String[]> getList(ArrayList<CollegeDetails> list) {
@@ -94,6 +166,10 @@ public class MedalsTallyActivity extends AppCompatActivity {
             @Override
             public int compare(CollegeDetails cd1, CollegeDetails cd2) {
                 // higher priority means first in the order. i.e. desc order of priority
+                // -1 implies order cd1 before cd2
+                // 1 implies cd2 before cd1
+                // 0 implies equal
+
                 if (cd1.getPriority() > cd2.getPriority()) return -1;
                 else if (cd1.getPriority() < cd2.getPriority()) return 1;
                 else return 0;
