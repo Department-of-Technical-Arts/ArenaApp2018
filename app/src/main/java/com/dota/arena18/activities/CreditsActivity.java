395a28e2180c76d5a6bab640bc4fa65176122406
Harshvardhan Takawale <harshvardhantakawale@gmail.com>
2018-01-19 23:43:19 +0530

    tint added

--- app/src/main/java/com/dota/arena18/activities/CreditsActivity.java
+++ app/src/main/java/com/dota/arena18/activities/CreditsActivity.java
@@ -1,5 +1,6 @@
 package com.dota.arena18.activities;
 
+import android.os.Build;
 import android.support.annotation.StringRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,6 +11,7 @@ import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 import com.dota.arena18.R;
+import com.jaouan.compoundlayout.CircleGradientRadioLayout;
 import com.jaouan.compoundlayout.CompoundLayout;
 import com.jaouan.compoundlayout.GradientRadioLayout;
 
@@ -34,9 +36,15 @@ public class CreditsActivity extends AppCompatActivity {
         description = findViewById(R.id.long_description);
         descriptionLayout = findViewById(R.id.description_layout);
 
-        bindCompoundListener((CompoundLayout) findViewById(R.id.profile_1), R.string.akhil, R.string.akhil);
-        bindCompoundListener((CompoundLayout) findViewById(R.id.profile_2), R.string.harshvardhan,R.string.harshvardhan);
-        bindCompoundListener((CompoundLayout) findViewById(R.id.profile_3), R.string.ashwik, R.string.ashwik);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            bindCompoundListener((CompoundLayout) findViewById(R.id.profile_1), R.string.akhil, R.string.akhil, (CircleGradientRadioLayout) findViewById(R.id.profile_1), getColor(R.color.circle2), getColor(R.color.circle2));
+            bindCompoundListener((CompoundLayout) findViewById(R.id.profile_2), R.string.harshvardhan, R.string.harshvardhan, (CircleGradientRadioLayout) findViewById(R.id.profile_2), getColor(R.color.circle3), getColor(R.color.circle4));
+            bindCompoundListener((CompoundLayout) findViewById(R.id.profile_3), R.string.ashwik, R.string.ashwik, (CircleGradientRadioLayout) findViewById(R.id.profile_3), getColor(R.color.circle5), getColor(R.color.circle6));
+        } else {
+            bindCompoundListener((CompoundLayout) findViewById(R.id.profile_1), R.string.akhil, R.string.akhil, (CircleGradientRadioLayout) findViewById(R.id.profile_1), 0, 0);
+            bindCompoundListener((CompoundLayout) findViewById(R.id.profile_2), R.string.harshvardhan, R.string.harshvardhan, (CircleGradientRadioLayout) findViewById(R.id.profile_2), 0,0);
+            bindCompoundListener((CompoundLayout) findViewById(R.id.profile_3), R.string.ashwik, R.string.ashwik, (CircleGradientRadioLayout) findViewById(R.id.profile_3), 0,0);
+        }
     }
 
     /**
@@ -45,7 +53,10 @@ public class CreditsActivity extends AppCompatActivity {
      * @param compoundLayout Compound layout.
      * @param subtitle       Subtitle to set.
      */
-    private void bindCompoundListener(final CompoundLayout compoundLayout, @StringRes final int subtitle,@StringRes final int desc) {
+    private void bindCompoundListener(final CompoundLayout compoundLayout, @StringRes final int subtitle, @StringRes final int desc
+            , final CircleGradientRadioLayout circleGradientRadioLayout
+            , final int color1
+            , final int color2) {
         compoundLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -59,7 +70,11 @@ public class CreditsActivity extends AppCompatActivity {
                     public void onAnimationEnd(Animation animation) {
                         subtitleTextView.setText(getString(subtitle));
                         description.setText(desc);
-                        descriptionLayout.startAnimation(AnimationUtils.loadAnimation(CreditsActivity.this, android.R.anim.fade_in));
+                        descriptionLayout.startAnimation(AnimationUtils.loadAnimation(CreditsActivity.this
+                                , android.R.anim.fade_in));
+                        circleGradientRadioLayout.setColorA(color1);
+                        circleGradientRadioLayout.setColorB(color2);
+                        circleGradientRadioLayout.setAngle(45);
                     }
 
                     @Override
