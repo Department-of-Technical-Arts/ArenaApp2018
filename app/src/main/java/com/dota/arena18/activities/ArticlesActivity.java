9c3b490479811aa32d2694be141cb2d74bf99faa
ashwik_aileni <ashwikreddyaileni999@gmail.com>
2018-01-20 15:32:59 +0530

    FB link for journal club

--- app/src/main/java/com/dota/arena18/activities/ArticlesActivity.java
+++ app/src/main/java/com/dota/arena18/activities/ArticlesActivity.java
@@ -1,11 +1,17 @@
 package com.dota.arena18.activities;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.dota.arena18.R;
 import com.dota.arena18.api.ArticleDetails;
@@ -24,6 +30,8 @@ public class ArticlesActivity extends AppCompatActivity {
 
     private RecyclerView mRecycler;
     private ArticlesAdapter mAdapter;
+    private ImageView jc_link;
+    private String download_link = "https://www.facebook.com/JournalClubBPHC/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +43,21 @@ public class ArticlesActivity extends AppCompatActivity {
 
         mRecycler = findViewById(R.id.articles_recycler);
         mRecycler.setLayoutManager(new LinearLayoutManager(this));
+        jc_link = findViewById(R.id.journal_club_link);
+
+        jc_link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(download_link));
+                    startActivity(myIntent);
+                } catch (ActivityNotFoundException e) {
+                    Toast.makeText(ArticlesActivity.this, "No application can handle this request."
+                            + " Please install a webbrowser",  Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+            }
+        });
 
         ArticlesInterface articles = TestApiClient.getClient().create(ArticlesInterface.class);
         Call<ArrayList<ArticleDetails>> call = articles.getArticlesList();
